43323dbf8bdfe8879315704836b6cf2ea193cc2b
Krzysiek <x.x.pl>
2014-05-09 16:37:30 +0200

    Dodano brakujący get oraz set dla pola password w ORM User

--- cudlax/src/main/java/pl/cudlax/domain/User.java
+++ cudlax/src/main/java/pl/cudlax/domain/User.java
@@ -87,6 +87,14 @@ public class User {
 		this.role = role;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", firstName=" + firstName
